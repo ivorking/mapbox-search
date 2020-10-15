ba6599fc5401113c3df661ca3c057378946b17d5
Ivor King <ivorking@gmail.com>
2020-10-15 07:36:53 +0100

    update

--- src/PlacesPanel.js
+++ src/PlacesPanel.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import PlaceItem from './PlaceItem'
 import './PlacesPanel.css'
 
 class PlacesPanel extends Component {
   render() {
+    const places = this.props.app.state.places
+    const placeItems = places.map(place => {
+      return <PlaceItem></PlaceItem>
+    })
+
     return (
       <div className= "places">
-        Places go here
+        {placeItems}
       </div>
     )
   }

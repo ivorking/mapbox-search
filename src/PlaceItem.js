a922409fc3322d9b4f062c53fc61ac5718b05644
Ivor King <ivorking@gmail.com>
2020-12-07 01:08:50 +0000

    update

--- src/PlaceItem.js
+++ src/PlaceItem.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react"
+import mapbox from 'mapbox-gl'
 
 class PlaceItem extends Component {
   render () {
+    const app = this.props.app
+    const map = app.state.map
+    console.log(map)
+
+    const place = this.props.place
+
+    if (map) {
+      console.log('yes map')
+      const marker = new mapbox.Marker({
+        color: '#2727eb6'
+      })
+    }
+
+    marker.setLngLat([place.longitude, place.latitude])
+    marker.addTo(map)
+
     return (
       <div className = "place-item">
-        {this.props.place.name} ({this.props.place.latitude}, {this.props.place.longitude})
+        {place.name} ({place.latitude}, {place.longitude})
       </div>
     )
   }

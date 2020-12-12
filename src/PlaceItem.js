925d90f0a76e8c9cc48206a209defd0293db2467
Ivor King <ivorking@gmail.com>
2020-12-12 22:54:34 +0000

    update

--- src/PlaceItem.js
+++ src/PlaceItem.js
@@ -2,6 +2,18 @@ import React, { Component } from "react"
 import mapbox from 'mapbox-gl'
 
 class PlaceItem extends Component {
+  
+  goTo() {
+    const app = this.props.app
+    const map = app.state.map
+    const place = this.props.place
+
+    map.flyTo({
+      center: [place.longitude, place.latitude],
+      zoom: 10
+    })
+  }
+  
   render () {
     const app = this.props.app
     const map = app.state.map
@@ -16,18 +28,18 @@ class PlaceItem extends Component {
 
       popup.setHTML(place.name)
 
-      console.log('yes map')
       const marker = new mapbox.Marker({
         color: '#2727e6'
       })
 
       marker.setLngLat([place.longitude, place.latitude])
+      marker.setPopup(popup)
       marker.addTo(map)
 
   }
 
     return (
-      <div className = "place-item">
+      <div className = "place-item" onClick = {() => this.goTo()}>
         {place.name} ({place.latitude}, {place.longitude})
       </div>
     )
